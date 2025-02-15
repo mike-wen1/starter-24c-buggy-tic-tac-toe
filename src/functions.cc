45f7a80a213250589c50ae3387ecea17727e4035
mike-wen1 <torchmaster123@gmail.com>
2025-02-15 07:55:17 +0000

    Reset driver.cc

--- src/functions.cc
+++ src/functions.cc
@@ -50,9 +50,9 @@ char CheckWinner(const std::vector<std::vector<char>>& board) {
   }
 
   if (board[0][0] == board[1][1] && board[1][1] == board[2][2] &&
-    board[2][2] != ' ') {
-  return board[0][0];
-}
+      board[2][2] != ' ') {
+    return board[0][0];
+  }
 
   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
       board[0][2] != ' ') {
